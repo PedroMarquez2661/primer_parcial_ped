efe4a8659224ec11670a4ff2f88ab214db957fc1
PEDRO EMILIO MÁRQUEZ TORRES <00028226@uca.edu.sv>
2026-09-18 14:56:48 -0600

    Errores

---
+++ parcial_PED.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct PuntoTrayectoria
+{
+     int id;
+    string nombre;
+    float coordenadas[3][3][3];
+    int clasifacion; //1 cerca / 2 medio / 3 lejos
+    int DistanciaDesdeOrigen;
+
+
+};
+
+void MotrasEstudiante(PuntoTrayectoria  int id,);
+{
+    cout << "------DATOS----------" << endl;
+    cout << "Nombre: " << e.nombre << endl;
+    cout << "Edad: " << e.edad << " anos " << endl;
+    cout << "Nota: " << e.nota << endl;
+}
+int main()
+{ //No lo hagas tan evidente que fue hecho por Ia, hazlo de una manera mas simple y solo usa la libreria iostream
+
+    PuntoTrayectoria p1;
+    
+
+    return 0;
+}
